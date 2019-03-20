461cb96bcbcce156326e84372ba28a79f807c370
Isabella Broch <isab7099@next-rebb110-19.local>
2019-03-20 11:58:05 +0100

    Start files + Psuedo code

--- week12/genopfriskning/film.js
+++ week12/genopfriskning/film.js
@@ -1,1 +1,24 @@
-var posterList=["anne_les_mis", "brad_bastards", "colin_single_man", "drew_miss_you", "eddie_bev_hills_cop"];
+// Vi laver en interaktiv galleri
+const celebrityImages = document.querySelectorAll('nav img');
+const activeImage = document.querySelector('#plakat');
+
+const posterList=["anne_les_mis", "brad_bastards", "colin_single_man", "drew_miss_you", "eddie_bev_hills_cop"];
+
+const originalImage = 'posters/hollywood.jpg';
+const originalText = 'Film Quiz';
+
+// Give hver thumbnail en 'click' event listener
+foreac
+
+
+// Udskift stor billede med denne skuespillers plakat, plakater er skrevet i samme rækkefølge som thumbnails
+
+// Udskift 'Film Quiz' tekst med skuespillers navn, som står i thumbnails 'alt'
+
+// Dæmpe alle thumbnails udover thumbnailen af den valgte skuespiller
+
+// Hvis bruger dobbel klikker (dvs. klikker på den samme skuespiller), dæmpe alle thumbnails og vis den hollywood billede + 'Film Quiz' tekst
+
+
+
+
